d7def9e4e6f7e307e860578970a6abb0d36e2822
ec3bd <ec3bd@virginia.edu>
2016-11-05 16:06:53 -0400

    Start for printing out optimal policy

--- ValueIteration.py
+++ ValueIteration.py
@@ -2,7 +2,7 @@ import sys
 import numpy
 
 def main():
-    MAX_ITERS = 1000
+    MAX_ITERS = 10
     Uprime = [[0 for x in range(7)] for y in range(7)]
     discount = 1
     iter = 0;
@@ -24,9 +24,23 @@ def main():
                 Uprime[i][j] = reward((i,j)) + discount*bestValue
                 if abs(Uprime[i][j] - U[i][j]) > delta:
                     delta = abs(Uprime[i][j] - U[i][j])
-        endCondition = delta < 0 or iter == MAX_ITERS
+        endCondition = delta < .009 or iter == MAX_ITERS
     print(numpy.asarray(Uprime))
+    print(iter)
 
+def optimalPolicy(startpoint, U):
+    i = startpoint[0]
+    j = startpoint[1]
+    while(True):
+        bestMove = (0,0)
+        bestValue = U[i][j]
+        for l in range(-1,2):
+            for k in range(-1,2):
+                if i+l < 7 and i+l >=0 and j+k < 7 and j+k >=0:
+                    if U[i+l][j+k] > bestValue:
+                        bestValue = U[i+1][j+k]
+                        bestMove = (l,k)
+        #delta =
 def reward(state):
     if state[0] is 3 and state[1] is 6:
         return 0

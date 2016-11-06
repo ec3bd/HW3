1f796271ec075050106f244bf6b517ad8a53defc
ec3bd <ec3bd@virginia.edu>
2016-11-06 01:15:23 -0400

    Delta correct; Wind cases implemented
    The delta condition terminates correctly, the list prints correctly, and
    the wind cases work correctly as moderated  by the booleans at the top.
    Left to program: display multiple optimal paths

--- ValueIteration.py
+++ ValueIteration.py
@@ -1,21 +1,32 @@
 import sys
 import numpy
+from copy import deepcopy
+
+
+MAX_ITERS = 50
+lightWind = False
+strongWind = True
 
 def main():
-    MAX_ITERS = 10
     Uprime = [[0 for x in range(7)] for y in range(7)]
     discount = 1
     iter = 0;
     endCondition = False
     while(not endCondition):
         iter+=1
-        U = Uprime
+        U = deepcopy(Uprime)
         delta = 0
         for i in range(0,7):
             for j in range(0,7):
                 bestValue = -sys.maxsize
-                bestMove = 0
-                for l in range(-1,2):
+                bestMove = (0,0)
+                wind = 0
+                if(j >= 3 and j <= 5):
+                    if lightWind:
+                        wind = -1
+                    elif strongWind:
+                        wind = -2
+                for l in range(-1+wind,2+wind):
                     for k in range(-1,2):
                         if i+l < 7 and i+l >=0 and j+k < 7 and j+k >=0:
                             if U[i+l][j+k] > bestValue:
@@ -24,23 +35,58 @@ def main():
                 Uprime[i][j] = reward((i,j)) + discount*bestValue
                 if abs(Uprime[i][j] - U[i][j]) > delta:
                     delta = abs(Uprime[i][j] - U[i][j])
-        endCondition = delta < 0 or iter == MAX_ITERS
+        endCondition = delta == 0 or iter == MAX_ITERS
+
+    numpy.set_printoptions(linewidth=90)
     print(numpy.asarray(Uprime))
     print(iter)
+    print(optimalPolicy((3,0), U, iter))
 
-def optimalPolicy(startpoint, U):
+def optimalPolicy(startpoint, U, optimal_iters = MAX_ITERS):
     i = startpoint[0]
     j = startpoint[1]
+    path = []
+    iter = 0
     while(True):
+        iter+=1
+        if iter == MAX_ITERS:
+            break
         bestMove = (0,0)
         bestValue = U[i][j]
-        for l in range(-1,2):
+        wind = 0
+        if(j >= 3 and j<=5):
+            if lightWind:
+                wind = -1
+            elif strongWind:
+                wind = -2
+        for l in range(-1+wind,2+wind):
             for k in range(-1,2):
                 if i+l < 7 and i+l >=0 and j+k < 7 and j+k >=0:
                     if U[i+l][j+k] > bestValue:
-                        bestValue = U[i+1][j+k]
+                        bestValue = U[i+l][j+k]
                         bestMove = (l,k)
-        #delta =
+        i = i + bestMove[0]
+        j = j + bestMove[1]
+        if bestMove == (0,0):
+            break
+        stringMove = ""
+        printMove = (bestMove[0]-wind, bestMove[1])
+        if printMove[0] == -1:
+            stringMove = "N"
+        elif printMove[0] == 0:
+            stringMove = ""
+        elif printMove[0] == 1:
+            stringMove = "S"
+        if printMove[1] == -1:
+            stringMove += "W"
+        elif printMove[1] == 0:
+            stringMove += ""
+        elif printMove[1] == 1:
+            stringMove += "E"
+        path.append(stringMove)
+
+    return path
+
 def reward(state):
     if state[0] is 3 and state[1] is 6:
         return 0
